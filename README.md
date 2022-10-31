1e39e8bbf79577b7d48f13e23f9e66adaf43fb61
LDudrey <113869466+LDudrey@users.noreply.github.com>
2022-10-31 14:40:06 -0500

    cleaned up js code added more special characters

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# Password-Generator
+# Password Generator
+Welcome to my JavaScript Password Generator! 
+
+## Description
+Passwords are a common part of day to day life. Most  As a future web developer I need a portfolio to showcase my skills and talent to future employers. An having a portfolio with deployed web applications is crucial to the success of my career search after bootcamp.
+
+## Usage
+
+https://ldudrey.github.io/Password-Generator/
+
+Below I'm showing the responsiveness of the webpage for varying devices. The images show the elements responding to decreasing size of viewscreens by stacking elements on top of each other. 
+
+
+
+## Credits
+
+Thanks to various tutorials from following websites:
+https://www.w3schools.com/
+
+https://developer.mozilla.org/en-US/
+
+https://stackoverflow.com
+
+An many thanks to tutor session with Alexis San Javier and TA assistance from Blessing Igbadumhe, Jose Centurion, Andrew Truong.
+
+## License
+Please refer to the LICENSE in the repo.
--- assets/script1.js
+++ assets/script1.js
@@ -1,8 +1,8 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-// https://www.ibm.com/support/pages/password-policy-and-passwords-special-characters
-// https://en.wikipedia.org/wiki/List_of_Special_Characters_for_Passwords
-var specialCase = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+
+// Arrays
+var specialCase = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "'", "+", ",", "-", ".", "/", ":", ";", "<", ">", "=", "?", "|", "{", "}", "[", "]", "_", "~", "`"];
 var integerCase = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
@@ -17,63 +17,53 @@ function writePassword() {
 
 }
 
-// ?? Should passwordLength be global variable. Have seperate function for get password length?
-
 // Creates prompts and alerts for password length
 function generatePassword() {
     var randomPassword = " ";
     var passwordLength = prompt("Choose a password length between 8 and 128 characters.")
     passwordLength = parseInt(passwordLength);
     if (passwordLength < 8 || passwordLength > 128) {
-        alert("Must be a number between 8 and 128. Please generate password again.");
+        alert("Must be a number between 8 and 128. Please click generate password again.");
         return;
     } else if (isNaN(passwordLength)) {
-        alert("Must be a number. Please generate password again");
+        alert("Must be a number. Please click generate password again");
         return;
     } 
     if (passwordLength === null) {
         return;
     }
-        //do you wish to continue? if canceled 
-    
-    // break? https://flexiple.com/javascript/javascript-exit-functions/
     // https://stackoverflow.com/questions/69018412/how-to-reload-a-prompt-if-user-selects-the-cancel-button-in-javascript
 
-    var lowChar = confirm("Should this password include lower case alphabet?");
+    // Confirm boxes for variables for characters
+    var lowChar = confirm("Click OK to include lower case alphabet.");
     if (lowChar === true) {
         masterCase = lowerCase.concat(masterCase);
-
-        //console.log(lowChar)
     }
-    var upChar = confirm("Should this password include upper case alphabet?");
+
+    var upChar = confirm("Click OK to include upper case alphabet.");
     if (upChar === true) {
         masterCase = upperCase.concat(masterCase);
     }
-        //console.log(upChar)
 
-    var intChar = confirm("Should this password include numbers?");
+    var intChar = confirm("Click OK to include numbers.");
     if (intChar === true) {
         masterCase = integerCase.concat(masterCase);
     }
-        //console.log(intChar)
 
-    var specChar = confirm("Should this password include special characters?");
+    var specChar = confirm("Click OK to include special characters.");
     if (specChar === true) {
         masterCase = specialCase.concat(masterCase);
     }
-        //console.log(specChar);
 
-    // if (lowChar = false && upChar = false && intChar = false && specChar = false) {
-    //     alert("You must select at least 1 character type to create password");
-    //     return generatePassword();
-    // }
+    if (!lowChar && !upChar && !intChar && !specChar) {
+        alert("You must select at least 1 character type to create password. Please click generate password again.");
+        return;
+    }
 
     // For loop to pick random characters
     for (i = 0; i < passwordLength; i++) {
         var index = Math.floor(masterCase.length * Math.random());
-        // console.log(index);
         randomPassword += masterCase[index];
-        // console.log(randomPassword);
     }
 
     return randomPassword;
